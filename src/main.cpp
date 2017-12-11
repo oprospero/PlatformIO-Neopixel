25110b0d97bc52cea834c579ac5296b11b48edf4
Prosper <vanprosper@gmail.com>
2017-12-10 19:02:35 -0800

    Added random animations

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 6
-#define COLOR_VARIATION 4
-#define CONTRAST_WIDTH 64
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -12,41 +9,47 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-int LEDs=60;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDs, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DATA_PIN 6
+const int numOfLeds=60;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numOfLeds, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
     strip.begin();
+
+    //Insure we don't pull to much power.
     strip.setBrightness(3);
 
-    for(int x=0;x<LEDs;x++){
+    // Turn led on one by one
+    for(int x=0;x<numOfLeds;x++){
         strip.setPixelColor(x,255,0,0);
         strip.show();
         delay(20);
     }
-    for(int x=0;x<LEDs;x++){
+    for(int x=0;x<numOfLeds;x++){
         strip.setPixelColor(x,0,255,0);
         strip.show();
         delay(20);
     }
-    for(int x=0;x<LEDs;x++){
+    for(int x=0;x<numOfLeds;x++){
         strip.setPixelColor(x,0,0,255);
         strip.show();
         delay(20);
     }
     strip.show();
 
-    for(int x=0;x<LEDs;x++){
+    for(int x=0;x<numOfLeds;x++){
         strip.setPixelColor(x,0,0,0);
     }
     strip.show();
 }
 void loop() {
     int previous, current = 0;
-    for(int x=LEDs - 1; x >= 0; x--){
+    for(int x=numOfLeds - 1; x >= 0; x--){
         strip.setPixelColor(previous,0,0,0);
-        int i = random(0,LEDs);
+        int i = random(0,numOfLeds);
         int g = random(0,255);
         int r = random(0,255);
         int b = random(0,255);
@@ -56,20 +59,20 @@ void loop() {
         previous = current;
         current = i;
     }
-    for(int x=0;x<LEDs;x++){
-    int g = random(0,255);
-    int r = random(0,255);
-    int b = random(0,255);
+    for(int x=0;x<numOfLeds;x++){
+        int g = random(0,255);
+        int r = random(0,255);
+        int b = random(0,255);
         strip.setPixelColor(x,g,r,b);
         strip.show();
         delay(25);
         strip.setPixelColor(x,0,0,0);
     }
 
-    for(int x=LEDs - 1; x >= 0; x--){
-    int g = random(0,255);
-    int r = random(0,255);
-    int b = random(0,255);
+    for(int x=numOfLeds - 1; x >= 0; x--){
+        int g = random(0,255);
+        int r = random(0,255);
+        int b = random(0,255);
         strip.setPixelColor(x,g,r,b);
         strip.show();
         delay(25);
@@ -77,8 +80,8 @@ void loop() {
     }
 
 
-    for(int x=LEDs - 1; x >= 0; x--){
-        int i = random(0,LEDs);
+    for(int x=numOfLeds - 1; x >= 0; x--){
+        int i = random(0,numOfLeds);
         int g = random(0,255);
         int r = random(0,255);
         int b = random(0,255);
@@ -88,18 +91,5 @@ void loop() {
         strip.setPixelColor(i,0,0,0);
     }
 
-    // strip.setPixelColor(11, 255, 0, 255);
-    // uint32_t magenta = strip.Color(255, 0, 255);
-    // strip.setPixelColor(7, magenta);
-    // uint32_t greenishwhite = strip.Color(0, 64, 0, 64);
-    // strip.show();
-    // uint32_t color = strip.getPixelColor(11);
-    // uint16_t n = strip.numPixels();
-    // for(int x=0;x<LEDs;x++){
-    //     strip.setPixelColor(x,255,255,255);
-    //     strip.show();
-    //     delay(50);
-    // }
-    // strip.show();
 
 }
